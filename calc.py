4f67ec1a78b206374aeb20c54506fb33800d423f
Praveen Naradwar <naradwarpraveen1997@gmail.com>
2025-11-15 14:24:21 +0530

    testing221

--- calc.py
+++ calc.py
@@ -8,7 +8,7 @@ def sub(a,b):
 def mul(a,b):
     return a*b
 
-def div(a,b):
-    if b==0:
-        raise ValueError ('cannot divide by zero')
-    return a/b
+# def div(a,b):
+#     if b==0:
+#         raise ValueError ('cannot divide by zero')
+#     return a/b
